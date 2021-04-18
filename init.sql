776bed43cd217cf9a8198b24c403124840ec7073
ilmars <9987548+darksworm@users.noreply.github.com>
2021-04-18 11:33:03 +0300

    split out nginx+fpm and swoole docker configs

--- init.sql
+++ init.sql
@@ -1,2 +1,7 @@
 CREATE DATABASE IF NOT EXISTS openchat;
 CREATE DATABASE IF NOT EXISTS openchat_test;
+
+CREATE USER 'foo'@'%' IDENTIFIED WITH mysql_native_password BY 'bar';
+GRANT ALL ON *.* to foo@'%' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
